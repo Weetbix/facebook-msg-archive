1e1e5c8538223e74299ece2c19e1eac328257918
John Hannagan <weetbix_@hotmail.com>
2016-11-01 23:06:27 +0100

    move messages to another module

--- bin/run.js
+++ bin/run.js
@@ -1,34 +1,15 @@
 var facebookAPI = require("facebook-chat-api");
-var friends = require( '../lib/friends' )
 var Q = require( "q" );
 var fs = require('fs');
 
-
-const MAX_MESSAGES = 10;
-const MESSAGES_PER_FETCH = 2;
+var friends = require( '../lib/friends' );
+var messages = require( '../lib/messages' );
 
 var credentials = 
 {
     email :  process.env.FACEBOOK_EMAIL,
     password : process.env.FACEBOOK_PASSWORD
 };
-
-function getMessages(api, userid, timestamp )
-{
-    return Q.nfcall(api.getThreadHistory, userid, 0, MESSAGES_PER_FETCH, timestamp );
-}
-
-async function getAllMessages(api, userid, timestamp = null, allMessages = [])
-{
-    while( allMessages.length < MAX_MESSAGES )
-    {
-        let messages = await getMessages( api, userid, timestamp );
-
-        allMessages = [ ... messages, ... allMessages ];      
-        timestamp    = messages[0].timestamp - 1;
-    }
-    return allMessages;
-}
 
 facebookAPI(
     credentials,
@@ -39,7 +20,7 @@ facebookAPI(
     friends.findFriendUserByName( api, "YOUR FRIEND HERE" )
     .then( user => 
     {
-        getAllMessages( api, user.id )
+        messages.getAllMessages( api, user.id )
         .then( messages => 
         {
             console.log( messages );
