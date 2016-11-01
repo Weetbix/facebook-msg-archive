e2a351d3f4d525cf74b896915b275ec510ba3b16
John Hannagan <weetbix_@hotmail.com>
2016-11-02 00:20:51 +0100

    Add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": [
+        "airbnb-base"
+    ],
+    "rules": {
+        "linebreak-style": ["error", "windows"],
+        "brace-style" : ["error", "allman", { "allowSingleLine": true } ],
+        "indent": [2, 4],
+        "comma-dangle": ["error", "never"]
+    }
+};
