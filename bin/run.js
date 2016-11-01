4a86767f7f6b4a0c1c44452d2daab07336eb8ba6
John Hannagan <weetbix_@hotmail.com>
2016-11-02 00:26:59 +0100

    fix eslint issues

--- bin/run.js
+++ bin/run.js
@@ -1,32 +1,32 @@
 const fs = require('fs');
-const facebookAPI = require("facebook-chat-api");
-const Q = require( "q" );
+const facebookAPI = require('facebook-chat-api');
+const Q = require('q');
 
-const Friends = require( '../lib/friends' );
-const Messages = require( '../lib/messages' );
+const Friends = require('../lib/friends');
+const Messages = require('../lib/messages');
 
 const OUTPUT_FILENAME = './output.json';
 
-const credentials = 
-{
-    email :  process.env.FACEBOOK_EMAIL,
-    password : process.env.FACEBOOK_PASSWORD
+const credentials = {
+    email: process.env.FACEBOOK_EMAIL,
+    password: process.env.FACEBOOK_PASSWORD
 };
 
-(async function(){
-    try 
+(async function run()
+{
+    try
     {
-        let api = await Q.nfcall( facebookAPI, credentials );
-        let friend = await Friends.findFriendUserByName( api, "YOUR FRIEND HERE" );
-        let messages = await Messages.getAllMessages( api, friend.id );
+        const api = await Q.nfcall(facebookAPI, credentials);
+        const friend = await Friends.findFriendUserByName(api, 'YOUR FRIEND HERE');
+        const messages = await Messages.getAllMessages(api, friend.id);
 
-        const formattedMessages = JSON.stringify( messages, null, 4 );
-        fs.writeFileSync( OUTPUT_FILENAME, formattedMessages );
+        const formattedMessages = JSON.stringify(messages, null, 4);
+        fs.writeFileSync(OUTPUT_FILENAME, formattedMessages);
 
-        console.log( 'Wrote to output' );
+        console.log('Wrote to output');
     }
-    catch( error )
+    catch (error)
     {
-        console.error( error.error || error );
+        console.error(error.error || error);
     }
-})();
+}());
